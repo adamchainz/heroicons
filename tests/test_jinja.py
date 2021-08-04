4e36f0710f370a6caf34b19405a7e1485c26623f
Adam Johnson <me@adamj.eu>
2021-08-04 09:03:28 +0100

    fix old python test

--- tests/test_jinja.py
+++ tests/test_jinja.py
@@ -62,15 +62,15 @@ def test_success_outline_changed_path_attr():
     result = template.render()
 
     expected_py37 = (
-        '<svg fill="none" height="24" '
-        + 'stroke="currentColor" viewBox="0 0 24 24" width="24">\n'
+        '<svg fill="none" height="24" stroke="currentColor" viewBox="0 0 24 24" '
+        + 'width="24">\n'
         + '  <path d="M12 14l9-5-9-5-9 5 9 5z" stroke-width="1" />\n'
         + '  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 '
         + "0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 "
-        + '14z" />\n'
+        + '14z" stroke-width="1" />\n'
         + '  <path d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 '
         + "6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 "
-        + '12.078 0 01.665-6.479L12 14zm-4 6v-7.5ln4-2.222" stroke-linecap="round" '
+        + '12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" stroke-linecap="round" '
         + 'stroke-linejoin="round" stroke-width="1" />\n'
         + "</svg>"
     )
