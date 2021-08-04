2f062f99dbd35c796abf34184a6a6cccc0ae88d2
Adam Johnson <me@adamj.eu>
2021-08-04 09:50:40 +0100

    Make some more functions “private” (#42)
    Just to check readers don't import and call them and form any expectations.

--- src/heroicons/jinja.py
+++ src/heroicons/jinja.py
@@ -4,12 +4,12 @@ import heroicons
 
 
 def heroicon_outline(name: str, *, size: int = 24, **kwargs: object) -> str:
-    return _heroicon("outline", name, size, **kwargs)
+    return _render_icon("outline", name, size, **kwargs)
 
 
 def heroicon_solid(name: str, *, size: int = 20, **kwargs: object) -> str:
-    return _heroicon("solid", name, size, **kwargs)
+    return _render_icon("solid", name, size, **kwargs)
 
 
-def _heroicon(style: str, name: str, size: int, **kwargs: object) -> str:
-    return Markup(heroicons.make_icon(style, name, size, **kwargs))
+def _render_icon(style: str, name: str, size: int, **kwargs: object) -> str:
+    return Markup(heroicons._render_icon(style, name, size, **kwargs))
--- tests/test_heroicons.py
+++ tests/test_heroicons.py
@@ -5,21 +5,21 @@ import pytest
 import heroicons
 
 
-def test_success_outline():
-    svg = heroicons.load_icon("outline", "academic-cap")
+def test_load_icon_success_outline():
+    svg = heroicons._load_icon("outline", "academic-cap")
     assert isinstance(svg, ElementTree.Element)
     assert svg.tag == ElementTree.QName("svg")
 
 
-def test_success_solid():
-    svg = heroicons.load_icon("solid", "academic-cap")
+def test_load_icon_success_solid():
+    svg = heroicons._load_icon("solid", "academic-cap")
     assert isinstance(svg, ElementTree.Element)
     assert svg.tag == ElementTree.QName("svg")
 
 
-def test_fail_unknown():
+def test_load_icon_fail_unknown():
     with pytest.raises(heroicons.IconDoesNotExist) as excinfo:
-        heroicons.load_icon("solid", "hoome")
+        heroicons._load_icon("solid", "hoome")
 
     assert excinfo.value.args == (
         "The icon 'hoome' with style 'solid' does not exist.",
