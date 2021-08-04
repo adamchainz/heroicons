1af3fc6307a09b7a6880d202d0d4206da6527724
Adam Johnson <me@adamj.eu>
2021-08-04 09:43:58 +0100

    For older pythons

--- src/heroicons/_compat.py
+++ src/heroicons/_compat.py
@@ -2,7 +2,7 @@ import sys
 
 if sys.version_info < (3, 9):
 
-    def str_removeprefix(self, prefix):
+    def str_removeprefix(self: str, prefix: str) -> str:
         if self.startswith(prefix):
             return self[len(prefix) :]
         else:  # pragma: no cover
