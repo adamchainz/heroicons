d0341a4ce3c67ca35af9b64ec9f84169161541c3
Adam Johnson <me@adamj.eu>
2022-10-07 15:58:03 +0100

    test fix

--- tests/test_jinja.py
+++ tests/test_jinja.py
@@ -146,7 +146,7 @@ def test_success_outline_size_none():
         " 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112"
         " 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000"
         " 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75"
-        ' 15.75v-1.5" stroke-linejoin="round" />\n</svg>'
+        ' 15.75v-1.5" stroke-linecap="round" stroke-linejoin="round" />\n</svg>'
     )
     expected_py38plus = (
         '<svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"'
