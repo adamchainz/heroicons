1c774579d89eb1a078860b1724a71bae52bc9b96
carpelli <92866289+carpelli@users.noreply.github.com>
2025-10-12 16:11:40 +0200

    pass kwargs on as attributes so that the style attribute doesn't clash with the style function argument

--- src/heroicons/jinja.py
+++ src/heroicons/jinja.py
@@ -5,21 +5,21 @@ from markupsafe import Markup
 import heroicons
 
 
-def heroicon_micro(name: str, *, size: int | None = 16, **kwargs: object) -> str:
-    return _render_icon("micro", name, size, **kwargs)
+def heroicon_micro(name: str, *, size: int | None = 16, **attributes: dict) -> str:
+    return _render_icon("micro", name, size, attributes)
 
 
-def heroicon_mini(name: str, *, size: int | None = 20, **kwargs: object) -> str:
-    return _render_icon("mini", name, size, **kwargs)
+def heroicon_mini(name: str, *, size: int | None = 20, **attributes: dict) -> str:
+    return _render_icon("mini", name, size, attributes)
 
 
-def heroicon_outline(name: str, *, size: int | None = 24, **kwargs: object) -> str:
-    return _render_icon("outline", name, size, **kwargs)
+def heroicon_outline(name: str, *, size: int | None = 24, **attributes: dict) -> str:
+    return _render_icon("outline", name, size, attributes)
 
 
-def heroicon_solid(name: str, *, size: int | None = 24, **kwargs: object) -> str:
-    return _render_icon("solid", name, size, **kwargs)
+def heroicon_solid(name: str, *, size: int | None = 24, **attributes: dict) -> str:
+    return _render_icon("solid", name, size, attributes)
 
 
-def _render_icon(style: str, name: str, size: int | None, **kwargs: object) -> str:
-    return Markup(heroicons._render_icon(style, name, size, **kwargs))
+def _render_icon(style: str, name: str, size: int | None, attributes: dict) -> str:
+    return Markup(heroicons._render_icon(style, name, size, attributes))
--- src/heroicons/templatetags/heroicons.py
+++ src/heroicons/templatetags/heroicons.py
@@ -9,31 +9,31 @@ register = template.Library()
 
 
 @register.simple_tag
-def heroicon_micro(name: str, *, size: int | None = 16, **kwargs: object) -> str:
-    return _render_icon("micro", name, size, **kwargs)
+def heroicon_micro(name: str, *, size: int | None = 16, **attributes: dict) -> str:
+    return _render_icon("micro", name, size, attributes)
 
 
 @register.simple_tag
-def heroicon_mini(name: str, *, size: int | None = 20, **kwargs: object) -> str:
-    return _render_icon("mini", name, size, **kwargs)
+def heroicon_mini(name: str, *, size: int | None = 20, **attributes: dict) -> str:
+    return _render_icon("mini", name, size, attributes)
 
 
 @register.simple_tag
-def heroicon_outline(name: str, *, size: int | None = 24, **kwargs: object) -> str:
-    return _render_icon("outline", name, size, **kwargs)
+def heroicon_outline(name: str, *, size: int | None = 24, **attributes: dict) -> str:
+    return _render_icon("outline", name, size, attributes)
 
 
 @register.simple_tag
-def heroicon_solid(name: str, *, size: int | None = 24, **kwargs: object) -> str:
-    return _render_icon("solid", name, size, **kwargs)
+def heroicon_solid(name: str, *, size: int | None = 24, **attributes: dict) -> str:
+    return _render_icon("solid", name, size, attributes)
 
 
-def _render_icon(style: str, name: str, size: int | None, **kwargs: object) -> str:
+def _render_icon(style: str, name: str, size: int | None, attributes: dict) -> str:
     # simple_tag's parsing loads passed strings as safe, but they aren't
     # Cast the SafeString's back to normal strings the only way possible, by
     # concatenating the empty string.
-    fixed_kwargs = {
+    fixed_attributes = {
         key: (value + "" if isinstance(value, SafeString) else value)
-        for key, value in kwargs.items()
+        for key, value in attributes.items()
     }
-    return mark_safe(heroicons._render_icon(style, name, size, **fixed_kwargs))
+    return mark_safe(heroicons._render_icon(style, name, size, fixed_attributes))
